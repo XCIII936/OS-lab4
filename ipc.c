752dda836b7ef47e1d5bb6b9671f884c5a0c4b90
lms <lms3082@outlook.com>
2025-05-27 15:10:35 +0800

    example all

--- ipc.c
+++ ipc.c
@@ -1,93 +1,110 @@
-/* 
- * Filename : ipc.c 
- * copyright : (C) 2006 by zhonghonglie 
- * Function : 一组建立 IPC 机制的函数 
-*/ 
-#include "ipc.h" 
-/* 
- * get_ipc_id() 从/proc/sysvipc/文件系统中获取 IPC 的 id 号
- * pfile: 对应/proc/sysvipc/目录中的 IPC 文件分别为
- * msg-消息队列,sem-信号量,shm-共享内存
- * key: 对应要获取的 IPC 的 id 号的键值
-*/ 
-int get_ipc_id(char *proc_file,key_t key) 
-{ 
- FILE *pf; 
- int i,j; 
- char line[BUFSZ],colum[BUFSZ]; 
- if((pf = fopen(proc_file,"r")) == NULL){ 
- perror("Proc file not open"); 
- exit(EXIT_FAILURE); 
- } 
- fgets(line, BUFSZ,pf); 
- while(!feof(pf)){ 
- i = j = 0; 
- fgets(line, BUFSZ,pf); 
- while(line[i] == ' ') i++; 
- while(line[i] !=' ') colum[j++] = line[i++]; 
- colum[j] = '\0'; 
- if(atoi(colum) != key) continue; 
- j=0; 
- while(line[i] == ' ') i++; 
- while(line[i] !=' ') colum[j++] = line[i++]; 
- colum[j] = '\0'; 
- i = atoi(colum); 
- fclose(pf); 
- return i; 
- } 
- fclose(pf); 
- return -1; 
-} 
-/* 
- * 信号灯上的 down/up 操作
- * semid:信号灯数组标识符
- * semnum:信号灯数组下标
- * buf:操作信号灯的结构
-*/ 
-int down(int sem_id) 
-{ 
- struct sembuf buf; 
- buf.sem_op = -1; 
- buf.sem_num = 0; 
- buf.sem_flg = SEM_UNDO; 
- if((semop(sem_id,&buf,1)) <0) { 
- perror("down error "); 
- exit(EXIT_FAILURE); 
- } 
- return EXIT_SUCCESS; 
-} 
-int up(int sem_id) 
-{ 
- struct sembuf buf; 
- buf.sem_op = 1; 
- buf.sem_num = 0; 
- buf.sem_flg = SEM_UNDO; 
- if((semop(sem_id,&buf,1)) <0) { 
- perror("up error "); 
- exit(EXIT_FAILURE); 
- } 
- return EXIT_SUCCESS; 
-} 
-/* 
-* set_sem 函数建立一个具有 n 个信号灯的信号量
-* 如果建立成功，返回 一个信号灯数组的标识符 sem_id 
-* 输入参数：
-* sem_key 信号灯数组的键值
-* sem_val 信号灯数组中信号灯的个数
-* sem_flag 信号等数组的存取权限
-*/ 
-int set_sem(key_t sem_key,int sem_val,int sem_flg) 
-{ 
- int sem_id; 
- Sem_uns sem_arg; 
- //测试由 sem_key 标识的信号灯数组是否已经建立
- if((sem_id = get_ipc_id("/proc/sysvipc/sem",sem_key)) < 0 ) 
- { 
- //semget 新建一个信号灯,其标号返回到 sem_id 
- if((sem_id = semget(sem_key,1,sem_flg)) < 0) 
- { 
- perror("semaphore create error"); 
- exit(EXIT_FAILURE); } 
- } 
- return msq_id; 
+#include "ipc.h"
+key_t buff_key;
+int buff_num;
+char *buff_ptr;
+key_t pput_key;
+int pput_num;
+int *pput_ptr;
+key_t cget_key;
+int cget_num;
+int *cget_ptr;
+key_t prod_key;
+key_t pmtx_key;
+int prod_sem;
+int pmtx_sem;
+key_t cons_key;
+key_t cmtx_key;
+int cons_sem;
+int cmtx_sem;
+int sem_val;
+int sem_flg;
+int shm_flg;
+int get_ipc_id(char *proc_file,key_t key) {
+    FILE *pf;
+    int i,j;
+    char line[BUFSZ],colum[BUFSZ];
+    if((pf = fopen(proc_file,"r")) == NULL){
+        perror("Proc file not open");
+        exit(EXIT_FAILURE);
+    }
+    fgets(line, BUFSZ,pf);
+    while(!feof(pf)){
+        i = j = 0;
+        fgets(line, BUFSZ,pf);
+        while(line[i] == ' ') i++;
+        while(line[i] !=' ') colum[j++] = line[i++];
+        colum[j] = '\0';
+        if(atoi(colum) != key) continue;
+        j=0;
+        while(line[i] == ' ') i++;
+        while(line[i] !=' ') colum[j++] = line[i++];
+        colum[j] = '\0';
+        i = atoi(colum);
+        fclose(pf);
+        return i;
+    }
+    fclose(pf);
+    return -1;
+}
+
+int down(int sem_id) {
+    struct sembuf buf;
+    buf.sem_op = -1;
+    buf.sem_num = 0;
+    buf.sem_flg = SEM_UNDO;
+    if(semop(sem_id,&buf,1) < 0) {
+        perror("down error ");
+        exit(EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
+}
+
+int up(int sem_id) {
+    struct sembuf buf;
+    buf.sem_op = 1;
+    buf.sem_num = 0;
+    buf.sem_flg = SEM_UNDO;
+    if(semop(sem_id,&buf,1) < 0) {
+        perror("up error ");
+        exit(EXIT_FAILURE);
+    }
+    return EXIT_SUCCESS;
+}
+
+int set_sem(key_t sem_key,int sem_val,int sem_flg) {
+    int sem_id;
+    Sem_uns sem_arg;
+    if((sem_id = get_ipc_id("/proc/sysvipc/sem",sem_key)) < 0 ) {
+        if((sem_id = semget(sem_key,1,sem_flg)) < 0) {
+            perror("semaphore create error");
+            exit(EXIT_FAILURE);
+        }
+        sem_arg.val = sem_val;
+        if(semctl(sem_id,0,SETVAL,sem_arg) < 0) {
+            perror("semaphore set error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return sem_id;
+}
+
+char * set_shm(key_t shm_key,int shm_num,int shm_flg) {
+    int i,shm_id;
+    char * shm_buf;
+    if((shm_id = get_ipc_id("/proc/sysvipc/shm",shm_key)) < 0 ) {
+        if((shm_id = shmget(shm_key,shm_num,shm_flg)) < 0) {
+            perror("shareMemory set error");
+            exit(EXIT_FAILURE);
+        }
+        if((shm_buf = (char *)shmat(shm_id,0,0)) < (char *)0) {
+            perror("get shareMemory error");
+            exit(EXIT_FAILURE);
+        }
+        for(i=0; i<shm_num; i++) shm_buf[i] = 0;
+    }
+    if((shm_buf = (char *)shmat(shm_id,0,0)) < (char *)0) {
+        perror("get shareMemory error");
+        exit(EXIT_FAILURE);
+    }
+    return shm_buf;
 }
---
+++ makefile
@@ -0,0 +1,26 @@
+hdrs = ipc.h
+opts = -g -c
+c_src = consumer.c
+c_obj = consumer.o ipc.o
+p_src = producer.c
+p_obj = producer.o ipc.o
+
+all: producer consumer
+
+consumer: $(c_obj)
+	gcc $(c_obj) -o consumer
+
+consumer.o: $(c_src) $(hdrs)
+	gcc $(opts) $(c_src)
+
+producer: $(p_obj)
+	gcc $(p_obj) -o producer
+
+producer.o: $(p_src) $(hdrs)
+	gcc $(opts) $(p_src)
+
+ipc.o: ipc.c $(hdrs)
+	gcc $(opts) ipc.c
+
+clean:
+	rm -f consumer producer *.o
